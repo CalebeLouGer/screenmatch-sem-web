f818fe611d858068367bee802e3268bff59b45c3
CalebeLouGer <calebelourenco581@gmail.com>
2025-11-27 16:18:41 -0400

    Alterando busca de episodios por ano

--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -8,6 +8,7 @@ import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -79,10 +80,17 @@ public class Principal {
         var ano = scanner.nextInt();
         scanner.nextLine();
         var anoAtual = 2025;
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         if (ano < anoAtual){
             LocalDate dataBusca = LocalDate.of(ano,1,1);
             episodios.stream()
-                    .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca));
+                    .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                    .forEach(e -> System.out.println(
+                            "Temporada: " + e.getTemporada() +
+                                    " | Data de Lançamento: " + e.getDataLancamento().format(formatador) +
+                                    " | Episódio: " + e.getTitulo()
+                    ));
         }else {
             System.out.println("Não passamos deste ano ainda!");
         }
